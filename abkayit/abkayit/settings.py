d349c56d114dcbc514ca1f81fff3e8ed72280e70
Ahmet Can KEPENEK <ahmetcan196@gmail.com>
2015-12-22 11:33:49 +0200

    locale middleware'i silindi - dil secenegi sadece turkce olacak.

--- abkayit/abkayit/settings.py
+++ abkayit/abkayit/settings.py
@@ -59,7 +59,6 @@ INSTALLED_APPS = (
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
