2186fff80fbfa59d4419136aead4cd336d598e1d
ackepenek <ahmetcan196@gmail.com>
2015-12-25 11:24:59 -0500

    table id'si duzenlenerek kurs secimindeki gereksiz javscript kodu cikarildi

--- abkayit/static/js/courses.js
+++ abkayit/static/js/courses.js
@@ -1,70 +1,22 @@
 $(document).ready(function(){
-	$('#course_table').DataTable({
-		 "language": {
-				"url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Turkish.json"
-         },
-		"searching": true,
-	    "ordering": true,
-		"paging" : false
-	});
 
-	$('#course_table tbody tr').click(function(){
+    $('[data-id="course_table"]').each(function(){
+        $(this).DataTable({
+             "language": {
+                    "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Turkish.json"
+             },
+            "searching": true,
+            "ordering": true,
+            "paging" : false
+        });
+    });
+
+	$('[data-id="course_table"]').click(function(){
 		var attr = 	$(this).attr('href');
 		if (typeof attr !== typeof undefined && attr !== false) {
 			window.location=attr;
 		}
 	});
-
-	$('.selectable').click(function () {
-	    var orderOneNotExists = $('.order-one.selected').size() === 0;
-	    var orderTwoNotExists = $('.order-two.selected').size() === 0;
-	    if ($(this).hasClass('selected')) {
-	        $(this).parent().parent().find('button').removeClass('selected');
-	        $(this).parent().parent().find('.selectable').find('i').removeClass('fa-thumbs-o-up');
-	        $(this).parent().parent().find('.selectable').find('i').addClass('fa-thumbs-o-down');
-	        if(!orderTwoNotExists){
-	            	$('.order-two.selected').parent().parent().find('.order-one').addClass('selected');
-	            	$('.order-two.selected').removeClass('selected');
-	        }
-	    } else {
-	        if (orderOneNotExists) {
-	            $(this).parent().parent().find('.order-one').addClass('selected');
-	            $(this).addClass('selected');
-	        	$(this).parent().parent().find('.selectable').find('i').removeClass('fa-thumbs-o-down');
-	        	$(this).parent().parent().find('.selectable').find('i').addClass('fa-thumbs-o-up');
-	        } else {
-	            if (orderTwoNotExists) {
-	                $(this).parent().parent().find('.order-two').addClass('selected');
-	                $(this).addClass('selected');
-	        		$(this).parent().parent().find('.selectable').find('i').removeClass('fa-thumbs-o-down');
-	        		$(this).parent().parent().find('.selectable').find('i').addClass('fa-thumbs-o-up');
-	            }
-	        }
-	    }
-	});
-	$('.order-one').click(function () {
-	    var notSelected = $(this).parent().parent().find('.selected').size() === 0;
-	    if (!notSelected) {
-	        if (!$(this).hasClass('selected')) {
-	            $('.order-one.selected').parent().find('.order-two').addClass('selected');
-	            $('.order-one.selected').removeClass('selected');
-	            $(this).addClass('selected');
-	            $(this).parent().find('.order-two').removeClass('selected');
-	        }
-	    }
-	});
-	$('.order-two').click(function () {
-	    var notSelected = $(this).parent().parent().find('.selected').size() === 0;
-	    if (!notSelected) {
-	        if (!$(this).hasClass('selected')) {
-	            $('.order-two.selected').parent().find('.order-one').addClass('selected');
-	            $('.order-two.selected').removeClass('selected');
-	            $(this).addClass('selected');
-	            $(this).parent().find('.order-one').removeClass('selected');
-	        }
-	    }
-	});
-
 
 
   $("#sendPreference").click(function(){
@@ -88,39 +40,4 @@ $(document).ready(function(){
   });
 
 
-	$('.apply-to-course').on('click', function(){
-		var jsonData = {};
-		var item = [];
-		var selectedItems = $("#course_table tbody tr th button i[class='fa fa-thumbs-o-up']");
-		selectedItems.each(function(){
-			var courseId = $(this).parent().attr('id').split('_')[1];
-			var preference = 0;
-			if ($("#button_"+courseId).parent().parent().find('.order-one.selected').size() ==1 ){
-				preference = "1";
-			}
-			else if ($("#button_"+courseId).parent().parent().find('.order-two.selected').size() ==1 ){
-				preference = "2";
-			}
-			
-			item.push('{"id":"'+courseId+'", "preference": "'+preference+'"}')	
-		});
-		jsonData['course'] = "[" + item.join(',') + "]";
-		jsonData['csrfmiddlewaretoken'] = getCookie('csrftoken');
-		console.log(JSON.stringify(jsonData));
-		$.ajax({
-		    url : "/egitim/applytocourse", 
-		    type : "POST",
-		    dataType: "json", 
-		    data : jsonData,
-		    success : function(json) {
-				alert(json.status);
-		    },
-		    error : function(xhr,errmsg,err) {
-		        alert(xhr.status + ": " + xhr.responseText);
-		    }
-		});
-
-	})
-})
-
-	;
+});
