708f72216465cda9d7553ddfc23925edf2f8c1e7
Özge Barbaros <ozgebarbaros@gmail.com>
2015-12-31 02:40:01 +0200

    egitmen ve egitim oluşturma betiği düzenlendi

--- abkayit/modelops.py
+++ abkayit/modelops.py
@@ -10,38 +10,53 @@ from abkayit.models import Site
 # user model fields: first_name, last_name, email = username 
 
 def savetrainers():
-    with open("sisteme_eklenecek_egitmenler.csv_bk") as e:
+    with open("sisteme_eklenecek_egitmenler.csv") as e:
         egitmenler = e.readlines()
         for egit in egitmenler:
             print egit
             cols=egit.split('|')
-            egitu = User(first_name=cols[0],last_name=cols[1],email=cols[4],username=cols[4])
-            egitu.set_password = '123456'
-            egitu.save()
-            egitup = UserProfile(user=egitu,
-                                 organization=cols[2],
-                                 tckimlikno='',
-                                 ykimlikno='',
-                                 gender='',
-                                 mobilephonenumber='',
-                                 address='',
-                                 job='',
-                                 city='',
-                                 title='',
-                                 university='',
-                                 department='',
-                                 country=cols[3],
-                                 is_instructor=True) 
-            egitup.save()
+            try:
+                egitu = User(first_name=cols[0],last_name=cols[1],email=cols[4].rstrip(),username=cols[4].rstrip())
+                egitu.set_password = '123456'
+                egitu.save()
+                egitup = UserProfile(user=egitu,
+                                     organization=cols[2],
+                                     tckimlikno='',
+                                     ykimlikno='',
+                                     gender='',
+                                     mobilephonenumber='',
+                                     address='',
+                                     job='',
+                                     city='',
+                                     title='',
+                                     university='',
+                                     department='',
+                                     country=cols[3],
+                                     is_instructor=True) 
+                egitup.save()
+                print "olustu"
+                print cols[4].rstrip()
+            except:
+                print "olusmadi"
+                print cols[4].rstrip()
 # no|name|description|url|trainers
 
 def savecourses():
-    with open("kurslar") as e:
+    with open("courses") as e:
         kurslar = e.readlines()
         for kurs in kurslar:
-            cols=kurs.split('|')
-            kursm = Course(no=cols[0],name=cols[1],description=cols[2],approved=True,site=Site.objects.get(is_active=True),url=cols[3])
-            kursm.save()
-            for egit in cols[4].split(','):
-                kursm.trainer.add(UserProfile.objects.get(user__username=egit.rstrip()))
-            kursm.save()
+            print kurs
+            try:
+                print "olustu"
+                cols=kurs.split('|')
+                kursm = Course(no=cols[0],name=cols[1],description='',
+                               approved=True,site=Site.objects.get(is_active=True),
+                               url="http://ab.org.tr/ab16/kursdir/"+str(cols[0])+".html")
+                kursm.save()
+                for egit in cols[2].split(','):
+                    print egit
+                    user=User.objects.get(username=egit.rstrip())
+                    kursm.trainer.add(UserProfile.objects.get(user=user))
+                kursm.save()
+            except:
+                print "olusmadi"
