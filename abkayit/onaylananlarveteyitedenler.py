d04076f046f4359c6f86f582580b042e51f0eedb
Ahmet Can Kepenek <ahmetcan196@gmail.com>
2016-01-18 01:06:53 +0200

    csv ureteci olan betikte ciktiya ek alanlar eklendi

--- abkayit/onaylananlarveteyitedenler.py
+++ abkayit/onaylananlarveteyitedenler.py
@@ -11,12 +11,20 @@ def sayilar():
     print "Onaylananlarin sayisi",len(countO)
     countT=TrainessCourseRecord.objects.filter(trainess_approved=True)
     print "Teyit edenlerin sayisi",len(countT)
-    for ct in countT:
+    for ct in countO:
         ctas=UserAccomodationPref.objects.filter(user=ct.trainess)
-        yazilacak = ct.trainess.user.first_name + ";" + ct.trainess.user.last_name  + ";" + ct.trainess.user.username + ";"
+        yazilacak = ct.trainess.user.first_name + ";" 
+        yazilacak  += ct.trainess.user.last_name  + ";" 
+        yazilacak += ct.trainess.user.username + ";"
+        yazilacak += (str(ct.trainess.gender) or "") + ";"
+        yazilacak += (ct.trainess.university or "") + ";"
+        yazilacak += (ct.trainess.organization or "") + ";"
+        yazilacak += (ct.course.name or  "") + ";"
+        yazilacak += str(ct.preference_order or  "") + ";"
+        yazilacak += str(ct.trainess_approved) + ";"
         if len(ctas)>0:
             for cta in ctas:
-                yazilacak += cta.accomodation.name + ";" + str(cta.preference_order)
+                yazilacak += (cta.accomodation.name or "") + ";" + (str(cta.preference_order) or "") + ";"
         else:
             yazilacak += ";"
         print yazilacak
