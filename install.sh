74dbe073d1bdf8391c868997f6c34da9b319639a
Özge Barbaros <ozgebarbaros@gmail.com>
2016-05-24 14:46:41 +0300

    kurulum scripti

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+pip install django==1.8.13
+pip install django-settings-context-processor
+pip install psycopg2
+pip install django-ckeditor
+pip install django-countries
+pip install pysimplesoap
+pip install django-longerusernameandemail
+pip install Pillow
