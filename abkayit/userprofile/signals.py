b8af336005869abd7a8797ee1d7b6fdd544a2ad3
Özge Barbaros <ozgebarbaros@gmail.com>
2016-06-23 16:56:01 +0300

    #83 abkayit altindaki signal'lar userprofile altına taşındı

--- abkayit/userprofile/signals.py
+++ abkayit/userprofile/signals.py
@@ -6,11 +6,12 @@ from django.contrib.auth.models import User
 from django.db.models import signals
 
 from abkayit.adaptor import send_email
-from abkayit.models import Site
+from abkayit.models import Site, ApprovalDate
 from abkayit.settings import EMAIL_FROM_ADDRESS
 from abkayit.backend import create_verification_link, send_email_by_operation_name
 from userprofile.models import UserVerification
-from mailing.models import EmailTemplate 
+from mailing.models import EmailTemplate
+from training.tutils import define_consentmailtotrainess_cronjob
 log = logging.getLogger(__name__)
 
 
@@ -27,4 +28,13 @@ def send_confirm_link(instance, created, **kwargs):
             send_email_by_operation_name(context, "send_activation_key")
 
 
+def definecronjob_signal(instance, created, **kwargs):
+    allapprovedates = ApprovalDate.objects.filter(for_instructor=True).order_by('-end_date')
+    if allapprovedates:
+        define_consentmailtotrainess_cronjob(allapprovedates[0])
+        log.info("consentmailtotrainess_cronjob defined for date %s" % allapprovedates[0].end_date.strftime(
+            "%Y-%m-%d %H:%M:%S"), extra={'clientip': '', 'user': ''})
+
+
 signals.post_save.connect(send_confirm_link, sender=User)
+signals.post_save.connect(definecronjob_signal, sender=Site)
