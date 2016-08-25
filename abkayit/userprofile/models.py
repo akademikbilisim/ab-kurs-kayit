b14cbeb2db9471d40f361cd54f254c41f468cdd4
Özge Barbaros <ozgebarbaros@gmail.com>
2016-08-25 16:03:42 +0300

    kullanıcı profili gösterimi ad, soyad email olarak değişti

--- abkayit/userprofile/models.py
+++ abkayit/userprofile/models.py
@@ -73,7 +73,7 @@ class UserProfile(models.Model):
     profilephoto = models.ImageField(upload_to=user_directory_path, verbose_name=_("Profile Picture"))
 
     def __unicode__(self):
-        return self.user.username
+        return self.user.first_name + " " + self.user.last_name + " " + self.user.username
 
     class Meta:
         ordering = ('user__username',)
