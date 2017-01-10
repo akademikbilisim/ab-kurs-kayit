0adc2347ea3634995fbc9db1dc4e4a1902cdb40b
ackepenek <ahmetcan.kepenek@gmail.com>
2017-01-10 11:39:00 +0300

    anketleri sorgulama problemi cozuldu

--- abkayit/surman/urls.py
+++ abkayit/surman/urls.py
@@ -1,9 +1,10 @@
 #!-*- coding:utf-8 -*-
 
 from django.conf.urls import patterns, url
+from django.contrib.auth.decorators import login_required
 
 from surman.views import AnswerListView
 
 urlpatterns = patterns('',
-                       url(r'^answers', AnswerListView.as_view(), name="survey_answers"),
+                       url(r'^answers', login_required(AnswerListView.as_view()), name="survey_answers"),
                        )
